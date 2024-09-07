d60c3a5635c4a9e01f1b0a018b3e1029f787135a
Ese Amadasun <amadasunese@gmail.com>
2024-09-07 11:30:54 +0200

    week 2 assignment updated

---
+++ applied_learning_assignment/week_2/dataframe.py
@@ -0,0 +1,15 @@
+# Import pandas
+import pandas as pd
+
+# Create python dictionary of name, age and country
+data = {'Name': ['Ese Amadasun', 'Treasure Amadasun', 'Evans Amadasun', 'Vanessa Amadasun'],
+        'Age': [48, 16, 14, 8],
+        'Country': ['Nigerian', 'United States', 'Canada', 'United Kingdom']
+        }
+
+# Convert the dictionary to a dataframe
+
+converted_df = pd.DataFrame(data)
+
+# Display the dataframe
+print(converted_df)
